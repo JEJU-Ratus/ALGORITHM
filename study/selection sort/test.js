e8a01e1b7454987438ee8568514f365af6f2e31f
jema <smh1141@naver.com>
2025-03-14 21:18:20 +0900

    좀 더 빠르게 수정

--- study/selection sort/test.js
+++ study/selection sort/test.js
@@ -2,17 +2,25 @@
 
 function SelectionSort() {
   let numArray = [1, 10, 5, 8, 7, 6, 4, 3, 2, 9];
-  let temp = null;
+  let temp = null,
+    index = null;
+
   for (let i = 0; i < numArray.length; i++) {
-    for (let j = i; j < numArray.length; j++) {
-      if (numArray[i] > numArray[j]) {
-        temp = numArray[i];
-        numArray[i] = numArray[j];
-        numArray[j] = temp;
+    let min = numArray[i];
+    index = i;
+    for (let j = i + 1; j < numArray.length; j++) {
+      if (min > numArray[j]) {
+        min = numArray[j];
+        index = j;
       }
     }
+    temp = numArray[i];
+    numArray[i] = min;
+    numArray[index] = temp;
   }
   console.log(numArray);
 }
 
 SelectionSort();
+
+// 선택 정렬은 원래 다른 정렬에 비해 비효율 적이지만, 그럼에도 효율을 위해 swap이 적게 일어나도록 해야함.(중요포인트)
